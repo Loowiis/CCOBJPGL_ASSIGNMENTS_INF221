5b76eed6327aebb74fcc0561ec9d9d3b31fcfbea
Salazar, Louis Francis <111473021+Loowiis@users.noreply.github.com>
2023-06-24 00:52:52 +0800

    Add files via upload

---
+++ Exercise3/Cat/Pet.java
@@ -0,0 +1,9 @@
+public class Pet implements Tonkinese {
+
+    @Override
+    public void meow() {
+
+        System.out.println("Meow");
+        System.out.println("My Tonkinese wants to play");
+    };
+}
